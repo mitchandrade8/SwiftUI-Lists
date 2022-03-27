ab885c06bb8d817ebaf78d54ce3dfd604ab85788
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-27 13:06:32 -0600

    third commit

---
+++ DisplayingItemsInList/Practice/FilteringResultsInAList.swift
@@ -0,0 +1,20 @@
+//
+//  FilteringResultsInAList.swift
+//  DisplayingItemsInList
+//
+//  Created by Mitch Andrade on 3/27/22.
+//
+
+import SwiftUI
+
+struct FilteringResultsInAList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilteringResultsInAList_Previews: PreviewProvider {
+    static var previews: some View {
+        FilteringResultsInAList()
+    }
+}
