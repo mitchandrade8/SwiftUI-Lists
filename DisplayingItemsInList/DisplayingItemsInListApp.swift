ffbfefe51f6d00f7b0f3775e2730df65c79fdd43
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-27 09:33:14 -0600

    Initial Commit

---
+++ DisplayingItemsInList/DisplayingItemsInListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayingItemsInListApp.swift
+//  DisplayingItemsInList
+//
+//  Created by Mitch Andrade on 3/27/22.
+//
+
+import SwiftUI
+
+@main
+struct DisplayingItemsInListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
