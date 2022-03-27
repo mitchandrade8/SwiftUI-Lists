93d7f4e8ac358ab765e1f2c3bfe9b4735d3ce02e
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-27 09:58:07 -0600

    first commit

---
+++ DisplayingItemsInList/Practice/HowToDisplayAlternateRowStylesInAList.swift
@@ -0,0 +1,20 @@
+//
+//  HowToDisplayAlternateRowStylesInAList.swift
+//  DisplayingItemsInList
+//
+//  Created by Mitch Andrade on 3/27/22.
+//
+
+import SwiftUI
+
+struct HowToDisplayAlternateRowStylesInAList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HowToDisplayAlternateRowStylesInAList_Previews: PreviewProvider {
+    static var previews: some View {
+        HowToDisplayAlternateRowStylesInAList()
+    }
+}
