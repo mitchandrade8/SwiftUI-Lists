a6c7facbd26eac0cf54fee6bd33bf7d81203f219
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-28 14:23:10 -0600

    fourth commit

---
+++ DisplayingItemsInList/Practice #2/HowToMakeTheCompleteRowClickable.swift
@@ -0,0 +1,20 @@
+//
+//  HowToMakeTheCompleteRowClickable.swift
+//  DisplayingItemsInList
+//
+//  Created by Mitch Andrade on 3/28/22.
+//
+
+import SwiftUI
+
+struct HowToMakeTheCompleteRowClickable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HowToMakeTheCompleteRowClickable_Previews: PreviewProvider {
+    static var previews: some View {
+        HowToMakeTheCompleteRowClickable()
+    }
+}
